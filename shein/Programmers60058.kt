c3a1e7d8d8c3fbc7b4b5f794b7da2db84daa8bdc
gosekose <ksyn1611@naver.com>
2024-03-13 21:22:51 +0900

    shein: 60058 카카오 재귀 & stack & 문자열 중복 코드 제거

--- shein/Programmers60058.kt
+++ shein/Programmers60058.kt
@@ -3,63 +3,7 @@ import java.util.*
 class Programmers60058 {
     fun solution(p: String): String {
         if (p == "") return "" // 조기 리턴
-        val stack = Stack<Char>()
-
-        for (s in p) {
-            if (stack.isEmpty()) stack.push(s)
-            if (stack.isNotEmpty()) {
-                if (stack.peek() == '(' && s == ')') {
-                    stack.pop()
-                } else stack.push(s)
-            }
-        }
-
-        if (stack.isEmpty()) return p
-
-        stack.clear()
-
-        // w, u, v 로 분리하기
-        var u = ""
-        var v = ""
-        var balanceU = ""
-        var isFirst = false
-        var uL = 0
-        var uR = 0
-        for (s in p) {
-            if (stack.isEmpty()) {
-                if (u != "") break // 올바른 문자열
-                stack.push(s)
-            } else {
-                if (stack.peek() == '(' && s == ')') {
-                    stack.pop()
-                } else stack.push(s)
-            }
-
-            if (s == '(') uL++
-            else uR++
-
-            u += s // 문자열 추가
-            if (uL != 0 && uL == uR && !isFirst) {
-                balanceU = u
-                isFirst = true
-            }
-        }
-
-        if (stack.isEmpty()) { // u가 올바른 문자열임
-            val tempV = p.slice(u.length..p.lastIndex)
-            return u + recur(tempV)
-        } else { // 올바르지 않은 문자열인 경우
-            // 균형잡힌 문자열 사용
-            u = balanceU
-            val tempV = p.slice(u.length..p.lastIndex)
-            var temp = "("
-            temp += recur(tempV) // v에 대한재귀 결과
-            temp += ")"
-            for (i in 1..u.lastIndex - 1) {
-                temp += if (u[i] == '(') ')' else '('
-            }
-            return temp
-        }
+        return recur(p)
     }
 
     private fun recur(p: String): String { // 리턴 값은 올바른 문자열이여야 함
@@ -89,7 +33,8 @@ class Programmers60058 {
             if (stack.isEmpty()) {
                 if (u != "") break // 올바른 문자열
                 stack.push(s)
-            } else {
+            }
+            else {
                 if (stack.peek() == '(' && s == ')') {
                     stack.pop()
                 } else stack.push(s)
@@ -106,7 +51,7 @@ class Programmers60058 {
         }
 
         if (stack.isEmpty()) {
-            val tempV = p.slice(u.length..p.lastIndex)
+            val tempV = p.slice(u.length .. p.lastIndex)
             for (t in tempV) { // 올바른 문자열 작업일 경우
                 if (stack.isNotEmpty()) {
                     if (stack.peek() == '(' && t == ')') {
@@ -124,11 +69,11 @@ class Programmers60058 {
             return u + recur(v)
         } else { // 올바르지 않은 문자열인 경우
             u = balanceU
-            val tempV = p.slice(u.length..p.lastIndex)
+            val tempV = p.slice(u.length .. p.lastIndex)
             var temp = "("
             temp += recur(tempV) // v에 대한재귀 결과
             temp += ")"
-            for (i in 1..u.lastIndex - 1) {
+            for (i in 1 .. u.lastIndex-1) {
                 temp += if (u[i] == '(') ')' else '('
             }
             return temp
@@ -144,4 +89,3 @@ class Programmers60058 {
 // 균형잡힌 문자열이 될떄까지 반복
 // u가 올바른 괄호 문자열이 아니라면,
 // 빈문자열에 첫번째 문자로 ( 를 붙임
-
